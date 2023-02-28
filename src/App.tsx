1348b15c4f69d27a567086b741ff4cc932aeb6d6
FZsiga02 <fzsigmond02@gmail.com>
2023-02-28 08:53:19 +0100

    Nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
+import Main from './components/pages/Main';
 
 class App extends React.Component {
 
   render() {
     return <div>
       <header>
-        Ide jön a navigáció
+        <nav>
+          <ul>
+            <li><Link to='/'>Főoldal</Link></li>
+            <li><Link to='/about'>Rólunk</Link></li>
+          </ul>
+        </nav>
       </header>
       <main>
         <Routes>
-          <Route path='/'element={<p>A főoldal tartalma</p>}/>
-          <Route path='/about'element={<p>Elérhetőség, rólunk stb.</p>}/>
+          <Route path='/' element={<Main />}/>
+          <Route path='/about' element={<p>Elérhetőség, rólunk stb.</p>}/>
+          <Route path='/user'>
+            <Route path='profile' element={<p>User profile</p>}/>
+            <Route path='settings' element={<p>User settings</p>}/>
+          </Route>
         </Routes>
       </main>
     </div>
